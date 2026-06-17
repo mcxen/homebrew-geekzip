464acc98c76b4823ff3bfb1a47d44f8933204cba
MCXEN <83444002+mcxen@users.noreply.github.com>
2026-06-17 19:48:23 +0800

    chore: update geekzip cask to v0.3.0

--- Casks/geekzip.rb
+++ Casks/geekzip.rb
@@ -1,19 +1,15 @@
 cask "geekzip" do
-  version "0.2.3"
-  sha256 "41c16db9a37abcd0cb761ac08250655fe6d738a0db594180f6e76c8ef14de8e8"
-
+  version "0.3.0"
+  sha256 "7d1a8846def34696f398bbda73b3e2914719b166819bd2624b4d8e31bd91e059"
   url "https://github.com/mcxen/GeekZip/releases/download/v#{version}/GeekZip-macos-arm64.app.zip"
   name "GeekZip"
   desc "Native smart archive compression and extraction tool"
   homepage "https://github.com/mcxen/GeekZip"
-
   livecheck do
     url :url
     strategy :github_latest
   end
-
   depends_on arch: :arm64
   depends_on macos: :monterey
-
   app "GeekZip.app"
 end
